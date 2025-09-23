574b2f455a6bfdfb892bfd0fbcb88330110df878
TQHYG <tqhyg@outlook.com>
2025-09-23 22:24:57 +0800

    deleted:    .gitmodules 	modified:   README.md 	deleted:    calc 	new file:   games/htmlchess/canvas3dengine/camera.jsfb 	new file:   games/htmlchess/canvas3dengine/light.jsfb 	new file:   games/htmlchess/canvas3dengine/matrix3.jsfb 	new file:   games/htmlchess/canvas3dengine/mesh.jsfb 	new file:   games/htmlchess/canvas3dengine/scene.jsfb 	new file:   games/htmlchess/canvas3dengine/vec3.jsfb 	new file:   games/htmlchess/chess.js 	new file:   games/htmlchess/common/xhr.js 	new file:   games/htmlchess/css/chess.css 	new file:   games/htmlchess/icons/backward.png 	new file:   games/htmlchess/icons/forward.png 	new file:   games/htmlchess/icons/go-next.png 	new file:   games/htmlchess/icons/go-previous.png 	new file:   games/htmlchess/icons/help-hint.png 	new file:   games/htmlchess/icons/play.png 	new file:   games/htmlchess/icons/reverse-play.png 	new file:   games/htmlchess/icons/skip-backward.png 	new file:   games/htmlchess/icons/skip-forward.png 	new file:   games/htmlchess/icons/stop.png 	new file:   games/htmlchess/index.html 	new file:   games/htmlchess/meshes/bishop.jscn 	new file:   games/htmlchess/meshes/board.json 	new file:   games/htmlchess/meshes/king.jscn 	new file:   games/htmlchess/meshes/knight.jscn 	new file:   games/htmlchess/meshes/pawn.jscn 	new file:   games/htmlchess/meshes/queen.jscn 	new file:   games/htmlchess/meshes/rook.jscn 	new file:   games/htmlchess/sample.pgn 	new file:   games/htmlchess/skins/gnomechess.css 	new file:   games/htmlchess/solidView.jsfb 	new file:   games/index.php 	new file:   games/sudoku.html

---
+++ games/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Kindle工具站</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/home.css">
+</head>
+<body>
+    <div class="top-bar">
+        <a href="javascript:history.back()" class="nav-btn">« 返回</a>
+        <a href="/" class="nav-btn"># 主页</a>
+        <span class="divider">|</span>
+        <div class="title-wrapper">
+            <h1 class="page-title">游戏</h1>
+        </div>
+    </div>
+
+    <h1 class="title">Kindle在线游戏</h1>
+    
+    <div class="hmenu">
+        <a href="htmlchess/" class="hbtn"><span>国际象棋</span></a>
+        <a href="sudoku.html" class="hbtn"><span>数独</span></a>
+    </div>
+
+</body>
+</html>
